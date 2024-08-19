42a91d5dce9274c86b22480072d2b8588ca25ae4
Your Name <na.phuongdx@gmail.com>
2024-08-19 23:29:50 +0700

    update

--- src/models/Product.js
+++ src/models/Product.js
@@ -56,12 +56,12 @@ const productSchema = new mongoose.Schema(
 
 productSchema.pre("save", function (next) {
     this.slug = slugify(this.name, {
-        replacement: "-", // replace spaces with replacement character, defaults to `-`
-        remove: undefined, // remove characters that match regex, defaults to `undefined`
-        lower: true, // convert to lower case, defaults to `false`
-        strict: false, // strip special characters except replacement, defaults to `false`
-        locale: "vi", // language code of the locale to use
-        trim: true, // trim leading and trailing replacement chars, defaults to `true`
+        replacement: "-", // Thay thế khoảng trắng bằng ký tự thay thế, mặc định là "-"
+        remove: undefined, // Loại bỏ các ký tự khớp với regex, mặc định là `undefined`
+        lower: true, // Chuyển đổi thành chữ thường, mặc định là `false`
+        strict: false, // Loại bỏ các ký tự đặc biệt ngoại trừ ký tự thay thế, mặc định là `false`
+        locale: "vi", // Mã ngôn ngữ của địa phương cần sử dụng
+        trim: true, // Loại bỏ khoảng trắng dẫn đầu và cuối ký tự thay thế, mặc định là `true`
     });
     next();
 });
